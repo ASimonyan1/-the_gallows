5bbe68fbc02e7bee89d2c6caba25f49acf9628bc
ASimonyan1 <artemsimonan81@gmail.com>
2023-07-02 19:14:27 +0300

    finish

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ def the_gallows(word):
     Sowpods = word
     fails = 0
     spisok = set()
-    a = len(word)
+    a = set(word)
     while fails < 10:
         n = input("введите букву: ")
-        if n in Sowpods:
+        if n in Sowpods and n not in spisok:
             spisok.add(n)
             print("одна из букв отгадана!")
+            print("отгаданные буквы:", spisok)
+        elif n in spisok:
+            print("вы уже отгадали эту букву!")
             print("отгаданные буквы:", spisok)
         else:
             fails += 1
             print("ошибок:", fails, "/10")
             print("неверно!!!")
-        if a == len(spisok):
+        if len(a) == len(spisok):
             print("ура победа!")
             print("загаданное слово: ", word)
             break
